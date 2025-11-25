860df2c4c5e4de4b1a4f9876f016f18a8c39a95e
ThaiBinh78 <154965828+ThaiBinh78@users.noreply.github.com>
2025-11-25 22:51:07 +0700

    Update teststreamlit.py

--- teststreamlit.py
+++ teststreamlit.py
@@ -1302,7 +1302,7 @@ elif st.session_state.current_page == "team":
 st.markdown("""
 <div style="text-align: center; color: #7f8c8d; padding: 40px 0 20px 0;">
     <hr style="border-color: #e0e6ed; margin-bottom: 20px;">
-    <p>MotorPrice Pro - Hệ thống dự đoán giá xe máy cũ | Phiên bản 1.0</p>
+    <b>MotorPrice Pro - Hệ thống dự đoán giá xe máy cũ | Phiên bản 1.0</b>
     <b> DATA SCIENCE AND MACHINE LEARNING COURSE</b><br>
     ĐỒ ÁN TỐT NGHIỆP DATA SCIENCE - MACHINE LEARNING<br>
 </div>
@@ -1331,6 +1331,7 @@ def show_footer():
         Email HV2: duythanh200620@gmail.com<br>
     </div>
     """, unsafe_allow_html=True)
+
 
 
 
